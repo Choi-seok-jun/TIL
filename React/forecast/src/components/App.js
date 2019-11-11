b0fa47000dfa95126af0f896205539159e43da03
Choi-seok-jun <50814945+Choi-seok-jun@users.noreply.github.com>
2019-11-11 10:03:29 +0900

    forest practice

--- React/forecast/src/components/App.js
+++ React/forecast/src/components/App.js
@@ -4,22 +4,25 @@ import Axios from "axios";
 import Current from "./Current";
 import Forecast from "./Forecast";
 import Spinner from "./Spinner";
+import Icons from "./Icons";
 
 import "./App.css";
 
 const App = () => {
-  const APPID = "4b8e16348db10af81b952f49bbb48318";
+  const APPID = "988033fdda81ccb3fa6a02cb6dab9c85";
   const [current, setCurrent] = useState(null);
   const [forecast, setForecast] = useState(null);
+  const [unit, setUnit] = useState("c");
 
   const getLocation = () => {
     return new Promise((resolve, reject) => {
       window.navigator.geolocation.getCurrentPosition(resolve, reject);
     });
   };
+
   const getTemp = async coords => {
     const { latitude: lat, longitude: lon } = coords;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${APPID}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${APPID}&units=metric&lang=kr`;
     const res = await Axios.get(url);
     const { data } = res;
     setCurrent(data);
@@ -42,7 +45,6 @@ const App = () => {
       alert("위치 제발 동의해주세요ㅠㅠ");
     }
   };
-
   useEffect(() => {
     getAll();
   }, []);
@@ -53,9 +55,15 @@ const App = () => {
         <h1>일기예보</h1>
       </header>
       <main className="container">
-        {!current ? <Spinner /> : <Current current={current} />}
-
-        <Forecast />
+        {!current || !forecast ? (
+          <Spinner />
+        ) : (
+          <>
+            <Current current={current} unit={unit} setUnit={setUnit} />
+            <Forecast forecast={forecast} unit={unit} />
+            <Icons />
+          </>
+        )}
       </main>
     </>
   );
---
+++ React/forecast/src/components/Icons.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Icons.css";
+import StateIcon from "./StateIcon";
+
+const Icons = props => {
+  return (
+    <>
+      <StateIcon />
+    </>
+  );
+};
+
+export default Icons;
